e09bd11cdba94b42cc31c80377f2d9329533fe05
manojkumar_m <M@noj10011988>
2021-02-10 01:20:19 -0600

    Added test case for github repository

--- Github-Data/src/test/java/com/github/data/repository/GitHubDataRepositoryTest.kt
+++ Github-Data/src/test/java/com/github/data/repository/GitHubDataRepositoryTest.kt
@@ -1,26 +1,99 @@
 package com.github.data.repository
 
+import androidx.test.platform.app.InstrumentationRegistry
+import com.github.data.api.APIConstants.GITHUB_URL
+import com.github.data.network.NetworkModule
 import com.github.data.network.RestClient
+import com.github.domain.DefaultObserver
+import com.github.domain.model.StarredRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import junit.framework.TestCase
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.runners.MockitoJUnitRunner
+import org.robolectric.RobolectricTestRunner
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
-@RunWith(MockitoJUnitRunner::class)
-class GitHubDataRepositoryTest {
+
+@RunWith(RobolectricTestRunner::class)
+class GitHubDataRepositoryTest : TestCase() {
 
     private var gitHubDataRepository:GitHubDataRepository? = null
-    private var gitHubDataRepository:GitHubDataRepository? = null
+    private val networkModule = NetworkModule()
 
     @Before
-    public  fun setUp() {
-        gitHubDataRepository = GitHubDataRepository(RestClient(NetworkModule.))
+    public override fun setUp() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        val restClient = RestClient(networkModule.retrofit(networkModule.okHttpClient(context!!), networkModule.url()))
+        gitHubDataRepository = GitHubDataRepository(restClient)
     }
 
-    public override fun tearDown() {}
+    @Test
+    fun testFetchMostStarredRepositoriesForIncorrectPageNumber() {
+        val countDownLatch =  CountDownLatch(1);
+        var exception:Throwable? = null
 
-    fun testFetchMostStarredRepositories() {}
+        val observable = gitHubDataRepository?.fetchMostStarredRepositories(100000000)
+        observable!!.subscribeOn(Schedulers.newThread())
+        observable.observeOn(AndroidSchedulers.mainThread())
+        observable.subscribeWith(object: DefaultObserver<StarredRepository>() {
+            override fun onError(e: Throwable?) {
+                exception = e
+            }
 
-    fun testGetRestClient() {}
+            override fun onNext(it: StarredRepository?) {
+            }
 
-    fun testSetRestClient() {}
+            override fun onComplete() {
+                super.onComplete()
+            }
+        })
+
+        countDownLatch.await(3000, TimeUnit.MILLISECONDS)
+        if(exception != null) {
+            Assert.fail(exception?.message)
+        }
+    }
+
+    @Test
+    fun testFetchMostStarredRepositories() {
+        val countDownLatch =  CountDownLatch(1);
+        var exception:Throwable? = null
+        var result:StarredRepository? = null
+        val observable = gitHubDataRepository?.fetchMostStarredRepositories(1)
+        observable!!.subscribeOn(Schedulers.newThread())
+        observable.observeOn(AndroidSchedulers.mainThread())
+        observable.subscribeWith(object: DefaultObserver<StarredRepository>() {
+            override fun onError(e: Throwable?) {
+                exception = e
+            }
+
+            override fun onNext(it: StarredRepository?) {
+                result = it
+            }
+
+            override fun onComplete() {
+                super.onComplete()
+            }
+        })
+        countDownLatch.await(3000, TimeUnit.MILLISECONDS)
+        assertTrue(result!!.repositoryModels.isNotEmpty())
+        result?.repositoryModels?.forEach { repository ->
+            assertNotNull(repository.repositoryName)
+            assertNotNull(repository.userName)
+            assertNotNull(repository.description)
+            assert(repository.numberOfStars > 0)
+        }
+
+    }
+
+    @Test
+    fun testGitHubUrl() {
+        assert(GITHUB_URL == networkModule.url())
+    }
+
+
 }
